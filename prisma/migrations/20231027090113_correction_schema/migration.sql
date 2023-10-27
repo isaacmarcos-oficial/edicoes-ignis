1c99b55c44fd11800661b22c9d82ab552bea7c99
Isaac Marcos <isaacmarcos@scdesign.com.br>
2023-10-27 06:03:58 -0300

    add seed script to prisma and correct schema

---
+++ prisma/migrations/20231027090113_correction_schema/migration.sql
@@ -0,0 +1,44 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `discountPercent` on the `Product` table. All the data in the column will be lost.
+
+*/
+-- CreateEnum
+CREATE TYPE "OrderStatus" AS ENUM ('WAITING_FOR_PAYMENT', 'PAYMENT_CONFIRMED');
+
+-- AlterTable
+ALTER TABLE "Product" DROP COLUMN "discountPercent",
+ADD COLUMN     "discountPercentage" INTEGER NOT NULL DEFAULT 0;
+
+-- CreateTable
+CREATE TABLE "Order" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "status" "OrderStatus" NOT NULL DEFAULT 'WAITING_FOR_PAYMENT',
+
+    CONSTRAINT "Order_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "OrderProduct" (
+    "id" TEXT NOT NULL,
+    "productId" TEXT NOT NULL,
+    "orderId" TEXT NOT NULL,
+    "basePrice" DECIMAL(8,2) NOT NULL,
+    "discountPercentage" INTEGER NOT NULL DEFAULT 0,
+    "quantity" INTEGER NOT NULL,
+
+    CONSTRAINT "OrderProduct_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OrderProduct" ADD CONSTRAINT "OrderProduct_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OrderProduct" ADD CONSTRAINT "OrderProduct_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,6 @@
-"use client"
-
-import { useSession } from "next-auth/react"
+"use client";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
-  const { data } = useSession()
-  
-  return (
-    <div>
-    </div>
-  )
+  return <div></div>;
 }
