6e2afcbce5cb30700042428510886c80eb4ff4de
Isaac Marcos <isaacmarcos@scdesign.com.br>
2023-10-27 06:52:54 -0300

    feat: add first discount banner to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,17 @@
 "use client";
-import { useSession } from "next-auth/react";
+import Image from "next/image";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="p-5">
+      <Image
+        src="/banner-home-01.png"
+        alt="até 55% de Desconto esse mês"
+        width={0}
+        height={0}
+        className="h-auto w-full"
+        sizes="100vw"
+      />
+    </div>
+  );
 }
