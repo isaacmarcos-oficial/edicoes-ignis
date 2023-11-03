461a2334db26fefac8f8a8da4fc46a5e2e5f42c0
Isaac Marcos <isaacmarcos@scdesign.com.br>
2023-11-03 07:24:40 -0300

    feat: link in category-page to category-items

--- src/app/(home)/components/category-item.tsx
+++ src/app/(home)/components/category-item.tsx
@@ -1,6 +1,7 @@
 import { Category } from "@prisma/client";
 import { Badge } from "@/components/ui/badge";
 import { CATEGORY_ICON } from "@/constants/category-icon";
+import Link from "next/link";
 
 interface CategoryItemProps {
   category: Category;
@@ -8,13 +9,15 @@ interface CategoryItemProps {
 
 const CategoryItem = ({ category }: CategoryItemProps) => {
   return (
-    <Badge
-      variant="outline"
-      className="py-3 flex justify-center items-center gap-2 rounded-lg"
-    >
-      {CATEGORY_ICON[category.slug as keyof typeof CATEGORY_ICON]}
-      <span className="font-bold text-xs">{category.name}</span>
-    </Badge>
+    <Link href={`/catalog/${category.slug}`} >
+      <Badge
+        variant="outline"
+        className="py-3 flex justify-center items-center gap-2 rounded-lg"
+      >
+        {CATEGORY_ICON[category.slug as keyof typeof CATEGORY_ICON]}
+        <span className="font-bold text-xs">{category.name}</span>
+      </Badge>
+    </Link>
   );
 };
 
